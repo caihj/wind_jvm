f1bdfbca2c30469506d69d9d4f906092ef8b76e8
gameworld <814130336@qq.com>
2018-06-28 00:52:20 +0800

    now jni can call

--- src/runtime/openJdkNative.cpp
+++ src/runtime/openJdkNative.cpp
@@ -67,66 +67,79 @@ void invoke_jni_func(const wstring & signature,void * func,list<Oop *> & stacks)
 }
 */
 
-bool loadNativeLib(std::wstring name){
+void initNativeInterface();
+struct __init{
+    __init(){
+        initNativeInterface();
+    }
+} ;
+static __init ___init;
+
+bool loadNativeLib(std::wstring name) {
 
     std::string libname = wstring_to_utf8(name);
 
-    void *handle = dlopen(libname.c_str(),RTLD_LAZY);
-    if(handle == nullptr){
+    void *handle = dlopen(libname.c_str(), RTLD_LAZY);
+    if (handle == nullptr) {
         return false;
     }
 
     {
         LockGuard l(Method_Pool::method_pool_lock());
         for (Method *method: Method_Pool::method_pool()) {
-            if(method->is_native() && method->getNative_method().empty()){
-                std::wcerr <<L"emtpy method "<< method->get_klass()->get_name()<<L":"<< method->get_name()<<std::endl;
+            if (method->is_native() && method->getNative_method().empty()) {
+                std::wcerr << L"emtpy method " << method->get_klass()->get_name() << L":" << method->get_name()
+                           << std::endl;
 
-                std::wcerr <<L"descriptor: "<<method->get_descriptor()<<std::endl;
+                std::wcerr << L"descriptor: " << method->get_descriptor() << std::endl;
 
-                wstring funcName = L"Java_" + std::regex_replace(method->get_klass()->get_name(), std::wregex(L"/"), L"_") + L"_" + method->get_name();
+                wstring funcName =
+                        L"Java_" + std::regex_replace(method->get_klass()->get_name(), std::wregex(L"/"), L"_") + L"_" +
+                        method->get_name();
                 std::wcerr << "funcName:" << funcName << std::endl;
 
-                void *funcPtr = dlsym(handle,wstring_to_utf8(funcName).c_str());
-                if(funcPtr != nullptr){
-                    std::wcerr <<  funcName << L" found!!!" <<  std::endl;
+                void *funcPtr = dlsym(handle, wstring_to_utf8(funcName).c_str());
+                if (funcPtr != nullptr) {
+                    std::wcerr << funcName << L" found!!!" << std::endl;
 
                     int argCount = method->parse_argument_list().size();
 
                     //for env and this
-                    argCount +=2;
+                    argCount += 2;
 
-                    method->setNative_method([argCount,funcPtr](list<Oop *> &_stack)-> void{
+                    method->setNative_method([argCount, funcPtr](list<Oop *> &_stack) -> void {
 
-                        ffi_type **ffiArgTypes = static_cast<ffi_type **>(alloca(sizeof(ffi_type *) *argCount));
-                        for(int i = 0 ;i < argCount; i++){
+                        ffi_type **ffiArgTypes = static_cast<ffi_type **>(alloca(sizeof(ffi_type *) * argCount));
+                        for (int i = 0; i < argCount; i++) {
                             ffiArgTypes[i] = &ffi_type_pointer;
                         }
 
-                        void **ffiArgs =  static_cast<void **>(alloca(sizeof(void *) *argCount));
+                        void **ffiArgs = static_cast<void **>(alloca(sizeof(void *) * argCount));
 
                         //env
-                        ffiArgs[0] = &nativeInterface ;
-                        for(int i = 1 ;i < argCount; i++){
-                            ffiArgs[i] = _stack.front(); _stack.pop_front();
+                        ffiArgs[0] = &nativeInterface;
+                        for (int i = 1; i < argCount; i++) {
+                            ffiArgs[i] = _stack.front();
+                            _stack.pop_front();
                         }
 
                         ffi_cif cif;
                         ffi_type *returnFfiType = &ffi_type_pointer;
 
-                        ffi_status ffiPrepStatus = ffi_prep_cif(&cif, FFI_DEFAULT_ABI, (unsigned int)argCount, returnFfiType, ffiArgTypes);
+                        ffi_status ffiPrepStatus = ffi_prep_cif(&cif, FFI_DEFAULT_ABI, (unsigned int) argCount,
+                                                                returnFfiType, ffiArgTypes);
 
                         if (ffiPrepStatus == FFI_OK) {
                             //生成用于保存返回值的内存
-                            void *returnPtr = NULL;
+                            void * *returnPtr = NULL;
                             if (returnFfiType->size) {
-                                returnPtr = alloca(returnFfiType->size);
+                                returnPtr = static_cast<void **>(alloca(returnFfiType->size));
                             }
                             //根据cif函数原型，函数指针，返回值内存指针，函数参数数据调用这个函数
                             ffi_call(&cif, reinterpret_cast<void (*)(void)>(funcPtr), returnPtr, ffiArgs);
 
                             //拿到返回值
-                            void * returnValue = (void *)returnPtr;
+                            void *returnValue = *returnPtr;
                             _stack.push_back(static_cast<Oop *>(returnValue));
                         }
 
@@ -142,15 +155,21 @@ bool loadNativeLib(std::wstring name){
 
 struct JNINativeInterface_ nativeInterface;
 
-void initNativeInterface(){
+void initNativeInterface() {
+    memset(&nativeInterface,0,sizeof(struct JNINativeInterface_));
+
+    JNIEnv *env = reinterpret_cast<JNIEnv *>(&nativeInterface);
+    env->functions = &nativeInterface;
     nativeInterface.NewStringUTF = &NewStringUTF;
 }
 
 
-jstring  NewStringUTF(JNIEnv *env,const char *utf){
+jstring NewStringUTF(JNIEnv *env, const char *utf) {
+
+    std::wcout << L"NewStringUTF called" << std::endl;
 
     wstring ws = utf8_to_wstring(std::string(utf));
 
-    return (jstring)java_lang_string::intern(ws);
+    return (jstring) java_lang_string::intern(ws);
 }
 
